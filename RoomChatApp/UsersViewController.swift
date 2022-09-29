f722d3df5d463256e9ef0a751d141539986d2f4d
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-09-29 23:54:26 +0200

    remove current user id from users tableView :)

--- RoomChatApp/UsersViewController.swift
+++ RoomChatApp/UsersViewController.swift
@@ -11,33 +11,33 @@ import FirebaseAuth
 class UsersViewController: UIViewController {
     
     @IBOutlet weak var usersTableView: UITableView!
+    let currentUserid = Auth.auth().currentUser?.uid
     var users: [User] = []
     var usersId = [String]()
-    let currentUserid = Auth.auth().currentUser?.uid
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Users"
         getUsers()
-        
         usersTableView.delegate = self
         usersTableView.dataSource = self
-        
-        
     }
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.navigationBar.prefersLargeTitles = false
     }
     //MARK: - private func
-    func getUsers(){
+    private func getUsers(){
         let ref = Database.database().reference()
-        ref.child("users").observe(.value) { snapshot in
+        ref.child("users").observe(.value) {snapshot in
             if let dataArray = snapshot.value as? [String: Any]{
                 self.usersId.append(contentsOf: dataArray.keys)
+                if let index = self.usersId.firstIndex(of: self.currentUserid!){
+                    self.usersId.remove(at: index)
+                }
                 self.getUsersNames()
             }
         }
     }
-    func getUsersNames(){
+    private func getUsersNames(){
         for id in usersId {
             let ref = Database.database().reference()
             ref.child("users").child(id).child("userName").observeSingleEvent(of: .value) { [weak self] snapShot in
@@ -51,6 +51,7 @@ class UsersViewController: UIViewController {
         }
     }
 }
+//MARK: - UITableViewDelegate
 extension UsersViewController: UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return users.count
@@ -59,8 +60,15 @@ extension UsersViewController: UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "usersCell")!
         cell.textLabel?.text = users[indexPath.row].name
+        cell.textLabel?.font = UIFont(name: "American Typewriter", size: 20)
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
 }
